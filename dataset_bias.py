56c3eb4df1ba45e4654a6d4d86ea0a722d2b7110
Rhea <bhutani.rhea@gmail.com>
2022-02-11 13:47:46 -0800

    preliminary iqa

--- dataset_bias.py
+++ dataset_bias.py
@@ -1,22 +1,30 @@
 import os
-
 from multinomial_pdf import *
+import imquality.brisque as brisque
+import PIL.Image
 
 dir = (os.listdir("datasets/part1"))
 
 GLOBAL = [.504, .496]
 
 distribution = [0.0, 0.0]
+imgQuals = [0.0, 0.0]
 
 for i in dir:
     label = i[:-4]
     label = label.split('_')
     distribution[1] += int(label[1])
 
-distribution[0] = len(dir) - distribution[1]
+    photo = PIL.Image.open("datasets/part1/" + i)
+    score = float(brisque.score(photo)) # might be a string, might be an int, not sure
+    imgQuals[int(label[1])] += score
 
-print(distribution[0] * 100 / len(dir), distribution[1] * 100 / len(dir))
-print(len(dir))
+distribution[0] = len(dir) - distribution[1]
+imgQuals = imgQuals/distribution
+
+print("representation: ", distribution[0] * 100 / len(dir), distribution[1] * 100 / len(dir))
+print("image quality indices: ", imgQuals)
+print("total sampled population: ", len(dir))
 
 
 bias = PDF(distribution, GLOBAL, len(dir))
