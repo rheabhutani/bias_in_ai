5d1e314da7c3e5def20549eb427cb03c56740c57
claudia valenta <claudiavalenta@claudias-MacBook-Pro-2.local>
2022-02-11 11:43:31 -0800

    new one

--- quality_index.py
+++ quality_index.py
@@ -1,8 +1,22 @@
-import os
+# https://pypi.org/project/image-quality/ for basic code to extract a brisque score from any image (lines 4-5, 10- )
+
 import imquality.brisque as brisque
 import PIL.Image
 
-dir = (os.listdir("Desktop/part3"))
+imgQuals = [0.0, 0.0]
+numPeople = [0, 0]
 
 for pic in dir:
-    photo = Image.PIL.open(pic)
+    photo = PIL.Image.open(pic)
+    score = brisque.score(photo) # might be a string, might be an int, not sure
+    
+    manOrWoman = int(label[1])
+
+    oldNum = imgQuals[manOrWoman] * numPeople[manOrWoman] # # of men or women based on previous % stored in imgQuals
+    numPeople[manOrWoman] += 1 # increment numPeople[0] by 1 if label[1] shows it's a man, otherwise increment women
+    imgQuals[manOrWoman] = (oldNum + score) / numPeople[manOrWoman] # find new average quality with new score added in
+
+if imgQuals[0] - imgQuals[1] > 0.05:
+    print('data is biased based on quality')
+else:
+    print('data is not biased based on quality')
